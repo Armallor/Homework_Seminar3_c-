214bff30d3688ad6271894587260abe86779ae37
Danil Muzafarov <d.muzafarov95@mail.ru>
2023-03-16 23:29:53 +0300

    Решение задачи по нахождению расстояния между точками с заданными координатами в 3D пространстве

---
+++ Quest_21/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние 
+// между ними в 3D пространстве
+
+int coordinat(string text)
+{
+  Console.Write(text);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+double distance(int[] x, int[] y, int[] z)
+{
+  return Math.Round(Math.Sqrt((Math.Pow((x[1]-x[0]), 2)) + (Math.Pow((y[1]-y[0]), 2)) + (Math.Pow((z[1]-z[0]), 2))), 2);
+}
+
+
+int[] x = new int[2];
+int[] y = new int[2];
+int[] z = new int[2];
+
+x[0] = coordinat("Введите значение координаты первой точки по оси x: ");
+y[0] = coordinat("Введите значение координаты первой точки по оси y: ");
+z[0] = coordinat("Введите значение координаты первой точки по оси z: ");
+x[1] = coordinat("Введите значение координаты второй точки по оси x: ");
+y[1] = coordinat("Введите значение координаты второй точки по оси y: ");
+z[1] = coordinat("Введите значение координаты второй точки по оси z: ");
+
+double S = distance(x, y, z);
+Console.Write($"Расстояние между точками равно {S}");

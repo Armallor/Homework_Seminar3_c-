5ae2e626de78c28924b223ab99acf2c95b475f56
Danil Muzafarov <d.muzafarov95@mail.ru>
2023-03-16 23:16:32 +0300

    Палиндром

---
+++ Quest_19/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом
+
+string Number(string text)
+{
+  Console.Write(text);
+  return (Console.ReadLine());
+}
+
+string chislo = Number("Введите пятизначное число: ");
+
+if ((chislo[0] == chislo[4]) && (chislo[1] == chislo[3])) Console.WriteLine("Ура, палиндром");
+
+else Console.WriteLine("Фу-х, непалиндром");
